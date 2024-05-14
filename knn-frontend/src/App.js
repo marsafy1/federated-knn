fa3f3576e46eb520e6dc958c4418fcbda8d0855b
marsafyyy <mokhallid72@gmail.com>
2024-05-15 01:09:14 +0300

    Added navbar + installed icons lib

--- knn-frontend/src/App.js
+++ knn-frontend/src/App.js
@@ -1,8 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">Hi</div>
+    <div className="App">
+      <Navbar/>
+    </div>
   );
 }
 
